521fe4d44af1c5263f80fa5f8f29de9a6653d6ff
Benoit Chauvet <contact@benoitchauvet.com>
2023-01-11 13:02:46 +0100

    first combinations attempts

--- nifty.py
+++ nifty.py
@@ -3,13 +3,18 @@ HEIGHT = 19
 
 grid = list()
 
-f = open("grid.txt", "r")
 
-line = f.readline()
-while line != "":
-    grid.append([*line.strip()])
+def init_grid():
+    grid = list()
+    f = open("grid.txt", "r")
+
     line = f.readline()
-f.close()
+    while line != "":
+        grid.append([*line.strip()])
+        line = f.readline()
+    f.close()
+    return grid
+
 
 red_curve = [
     [0, 0],
@@ -136,6 +141,13 @@ striped_curve = [
     [15, 12],
 ]
 
+curves = {
+    "red": red_curve,
+    "yellow": yellow_curve,
+    "brown": brown_curve,
+    "striped": striped_curve,
+}
+
 
 def draw_grid():
     for i in range(WIDTH):
@@ -158,6 +170,13 @@ def put(curve, x_start, y_start, sign):
         x = point[0] + x_start
         y = point[1] + y_start
         grid[x][y] = sign
+
+
+def delete(curve, x_start, y_start):
+    for point in curve:
+        x = point[0] + x_start
+        y = point[1] + y_start
+        grid[x][y] = "."
 
 
 def flip_h(curve):
@@ -185,77 +204,114 @@ def flip_v_and_h(curve):
     return result
 
 
-def try_reds():
-    possible_reds = list()
+def get_possible_starts(curve):
+    possible_starts = list()
 
-    red_width = red_curve[len(red_curve) - 1][0]
-    red_height = red_curve[len(red_curve) - 1][1]
+    curve_width = curve[len(curve) - 1][0]
+    curve_height = curve[len(curve) - 1][1]
 
-    for i in range(WIDTH - red_width):
-        for j in range(HEIGHT - red_height):
-            if fits(red_curve, i, j):
-                possible_reds.append([i, j])
+    for i in range(WIDTH - curve_width):
+        for j in range(HEIGHT - curve_height):
+            if fits(curve, i, j):
+                possible_starts.append([i, j])
 
-    print("possible red starts: ")
-    print(possible_reds)
-
-
-def try_yellows():
-    possible_yellows = list()
-
-    yellow_width = yellow_curve[len(yellow_curve) - 1][0]
-    yellow_height = yellow_curve[len(yellow_curve) - 1][1]
-
-    for i in range(WIDTH - yellow_width):
-        for j in range(HEIGHT - yellow_height):
-            if fits(yellow_curve, i, j):
-                possible_yellows.append([i, j])
-
-    print("possible yellow starts: ")
-    print(possible_yellows)
+    # print("possible starts: ")
+    # print(possible_starts)
+    return possible_starts
 
 
-def try_browns():
-    possible_browns = list()
-
-    brown_width = brown_curve[len(brown_curve) - 1][0]
-    brown_height = brown_curve[len(brown_curve) - 1][1]
-
-    for i in range(WIDTH - brown_width):
-        for j in range(HEIGHT - brown_height):
-            if fits(brown_curve, i, j):
-                possible_browns.append([i, j])
-
-    print("possible brown starts: ")
-    print(possible_browns)
-
-
-def try_striped():
-    possible_striped = list()
-
-    striped_width = striped_curve[len(striped_curve) - 1][0]
-    striped_height = striped_curve[len(striped_curve) - 1][1]
-
-    for i in range(WIDTH - striped_width):
-        for j in range(HEIGHT - striped_height):
-            if fits(striped_curve, i, j):
-                possible_striped.append([i, j])
-
-    print("possible striped starts: ")
-    print(possible_striped)
+def try_combination(curve_1, curve_2, curve_3, curve_4):
+    first_curve_starts = get_possible_starts(curve_1)
+    for start_1 in first_curve_starts:
+        # print(f"lvl1 : {start_1}")
+        put(curve_1, start_1[0], start_1[1], "1")
+        second_curve_starts = get_possible_starts(curve_2)
+        for start_2 in second_curve_starts:
+            # print(f"lvl2 : {start_2}")
+            put(curve_2, start_2[0], start_2[1], "2")
+            third_curve_starts = get_possible_starts(curve_3)
+            for start_3 in third_curve_starts:
+                # print(f"lvl3 : {start_3}")
+                put(curve_3, start_3[0], start_3[1], "3")
+                fourth_curve_starts = get_possible_starts(curve_4)
+                if len(fourth_curve_starts) > 0:
+                    print("SUCCESS !!!")
+                # else:
+                # print("nope...")
+                delete(curve_3, start_3[0], start_3[1])
+            delete(curve_2, start_2[0], start_2[1])
+        delete(curve_1, start_1[0], start_1[1])
 
 
-put(red_curve, 0, 6, "1")
-
-put(flip_v(red_curve), 2, 0, "2")
+grid = init_grid()
 
 # put(yellow_curve, 2, 0, "y")
 # put(brown_curve, 1, 3, "b")
 
-draw_grid()
+for a in range(len(curves)):
+    key_a = list(curves.keys())[a]
+    for b in range(len(curves)):
+        if b != a:
+            key_b = list(curves.keys())[b]
+            for c in range(len(curves)):
+                if c != a and c != b:
+                    key_c = list(curves.keys())[c]
+                    for d in range(len(curves)):
+                        if d != a and d != b and d != c:
+                            key_d = list(curves.keys())[d]
+                            # print(f"{key_a} {key_b} {key_c} {key_d} ")
+                            try_combination(
+                                curves.get(key_a),
+                                curves.get(key_b),
+                                curves.get(key_c),
+                                curves.get(key_d),
+                            )
+
+
+# draw_grid()
+
 
 # cases :
 # base
 # flip_h
 # flip_v
 # flip_h + flip_v
+
+# 4 base
+# 4 flip_h
+# 4 flip_v
+# 4 flip_v_and_h
+
+# 3 base + 1 flip_h
+# 3 base + 1 flip_v
+# 3 base + 1 flip_v_and_h
+
+# 2 base + 2 flip_h
+# 2 base + 2 flip_v
+# 2 base + 2 flip_v_and_h
+
+# 2 base + 1 flip_v
+
+
+# from itertools import permutations, combinations
+
+# elements = ['a', 'b', 'c', 'd']
+# states = ['e1', 'e2', 'e3', 'e4']
+
+# # Génération de toutes les combinaisons d'éléments
+# combinations = list(combinations(elements, 4))
+
+# # Génération de toutes les combinaisons d'états
+# state_combinations = list(combinations(states, 4))
+
+# # Génération de toutes les permutations d'éléments
+# permutations = list(permutations(elements))
+
+# # Génération de toutes les permutations d'états
+# state_permutations = list(permutations(states))
+
+# # Génération de toutes les combinaisons d'éléments et d'états
+# combination_and_permutations = []
+# for c in combinations:
+#     for s in state_combinations:
+#         combination_and_permutations.append(list(zip(c,s)))
